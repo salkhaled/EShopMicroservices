00e6dd971b34465f526597248f45ab6c87dfb553
salkhaled <salkhaled@bhs-intralogstics.com>
2025-05-23 13:29:38 +0200

    WIP: distributed cahcing (Caching aside pattern) & proxy pattern with decorators

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -16,9 +16,16 @@ builder.Services.AddMarten(opts =>
 })
     .UseLightweightSessions();
 
-builder.Services.AddExceptionHandler<CustomExceptionHandler>();
-
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 
