4c6d9575f81ff68266462f07b8aebb6e93066203
SAlKhaled <SAlKhaled@bhs-intralogistics.com>
2025-06-27 10:05:04 +0200

    Fix not found status code

--- src/BuildingsBlocks/BuildingsBlocks/Exceptions/Handler/CustomExceptionHandler.cs
+++ src/BuildingsBlocks/BuildingsBlocks/Exceptions/Handler/CustomExceptionHandler.cs
@@ -20,7 +20,7 @@ public class CustomExceptionHandler
         {
             InternalServerException => (exception.Message, exception.GetType().Name, httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError),
             ValidationException or BadRequestException => (exception.Message, exception.GetType().Name, httpContext.Response.StatusCode = StatusCodes.Status400BadRequest),
-            NotFoundException => (exception.Message, exception.GetType().Name, httpContext.Response.StatusCode = StatusCodes.Status400BadRequest),
+            NotFoundException => (exception.Message, exception.GetType().Name, httpContext.Response.StatusCode = StatusCodes.Status404NotFound),
             _ => (exception.Message, exception.GetType().Name, httpContext.Response.StatusCode = StatusCodes.Status400BadRequest),
         };
 
