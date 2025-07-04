d4069b726cc2aaa4e8900de42dda789b94c5b338
SAlKhaled <SAlKhaled@bhs-intralogistics.com>
2025-07-04 14:08:58 +0200

    WIP: Discount grpc ms

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            // TODO: Get Discount from Database
+            return base.CreateDiscount(request, context);
+        }
+
+        public override Task<CouponModel> UpdatetDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            return base.UpdatetDiscount(request, context);
+        }
+
+        public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            return base.DeleteDiscount(request, context);
+        }
+
+        public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            return base.GetDiscount(request, context);
+        }
+    }
+}
