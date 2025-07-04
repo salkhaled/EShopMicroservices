8f0d01315cfd5572ff1b80521e68ce32faac7363
SAlKhaled <SAlKhaled@bhs-intralogistics.com>
2025-07-04 14:08:58 +0200

    WIP: Discount grpc ms

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,39 @@
+﻿using Discount.Grpc.Data;
+using Discount.Grpc.Models;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Services;
+
+public class DiscountService
+    (DiscountContext dbContext, ILogger<DiscountService> logger) : DiscountProtoService.DiscountProtoServiceBase
+{
+    public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    {
+        // TODO: Get Discount from Database
+        return base.CreateDiscount(request, context);
+    }
+
+    public override Task<CouponModel> UpdatetDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    {
+        return base.UpdatetDiscount(request, context);
+    }
+
+    public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    {
+        return base.DeleteDiscount(request, context);
+    }
+
+    public async override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = await dbContext
+            .Coupons.FirstOrDefaultAsync(c => c.ProductName == request.ProductName);
+
+        coupon ??= new Coupon { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
+
+        logger.LogInformation("Discount is retrieved for ProductName : {prductName}, Amount : {amount}", coupon.ProductName, coupon.Amount);
+
+        return coupon.Adapt<CouponModel>();
+    }
+}
